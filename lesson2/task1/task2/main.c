b82a4bf08b17c61c744b49b1eb75f51c767815d1
Sidddha <nikitaprudov@gmail.com>
2023-12-14 18:09:06 +0300

    add task2

---
+++ lesson2/task1/task2/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num1 = 0;
+    int num2 = 0;
+    int num3 = 0;
+    printf("Input three numbers:\n");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    int sum = num1+num2+num3;
+    int mult = num1*num2*num3;
+    printf("%d+%d+%d=%d\n", num1, num2, num3, sum);
+    printf("%d*%d*%d=%d\n", num1, num2, num3, mult);
+    return 0;
+}
