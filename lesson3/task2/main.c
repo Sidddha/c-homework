cff80573951c82637cd8532c0c862501d20accbf
Sidddha <nikitaprudov@gmail.com>
2023-12-23 15:23:38 +0300

    add task2

--- lesson3/task2/main.c
+++ lesson3/task2/main.c
@@ -3,15 +3,15 @@
 int main(int argc, char const *argv[])
 {
     int nums[5];
-    int max = 0;
+    int min = 0;
     printf("Input five numbers:\n");
     scanf("%d%d%d%d%d", &nums[0], &nums[1], &nums[2], &nums[3], &nums[4]);
 
 
     for(int i = 0; i < 5; i++) {
-        if(max < nums[i])
-            max = nums[i];
+        if(min > nums[i])
+            min = nums[i];
     }
-    printf("%d\n", max);
+    printf("%d\n", min);
     return 0;
 }
